4665579cae275f8e8e4ec7f75ef9b961ea110413
Nafissah Pouye <Nafissah Pouye>
2019-10-21 12:16:09 +0000

    initial commit

---
+++ code.R
@@ -0,0 +1,134 @@
+#Load required libraries
+library(readxl)      #for excel, csv sheets manipulation
+library(sdcMicro)    #sdcMicro package with functions for the SDC process 
+library(tidyverse)   #optional #for data cleaning
+
+#Import data
+setwd("C:/Users/LENOVO T46OS/Desktop/C:/Users/LENOVO T46OS/Desktop/sdc-bgd-mcna-r2-hh-level")
+data <- read_excel("data.xlsx", sheet = "Dataset - Household", 
+                   col_types = c("text", "numeric", "text", 
+                                 "text", "numeric", "text", "text", 
+                                 "numeric", "text", "text", "text", 
+                                 "numeric", "text", "numeric", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "text", "text", "text", "text", "text", 
+                                 "numeric"))
+#Select key variables                   
+selectedKeyVars <- c('enum_gender',	'upazila_name', 'union_name',
+                     'respondent_age', 'respondent_gender',
+                     'respondent_hoh',	'hoh_gender',
+                     'hoh_age',	'hoh_marital',	'hh_size')
+
+#select weights
+weightVars <- c('weights')
+
+#Convert variables into factors
+cols =  c('enum_gender',	'upazila_name', 'union_name',
+          'respondent_age', 'respondent_gender',
+          'respondent_hoh',	'hoh_gender',
+          'hoh_age',	'hoh_marital',	'hh_size')
+
+data[,cols] <- lapply(data[,cols], factor)
+
+#Convert the sub file into a dataframe
+subVars <- c(selectedKeyVars, weightVars)
+fileRes<-data[,subVars]
+fileRes <- as.data.frame(fileRes)
+objSDC <- createSdcObj(dat = fileRes, 
+                       keyVars = selectedKeyVars
+                       )
+
+#print the risk
+print(objSDC, "risk")
+max(objSDC@risk$global[, "risk"])
+
+#Generate an internal (extensive) report
+report(objSDC, filename = "index",internal = T, verbose = TRUE) 
+
+
